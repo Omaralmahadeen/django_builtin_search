57782963e766cde32ebaf8ea28d4025fa088010c
Omar Almahadeen <69389320+Omaralmahadeen@users.noreply.github.com>
2023-08-12 19:55:31 +0300

    Update views.py

--- views.py
+++ views.py
@@ -4,14 +4,18 @@ from django.db.models import Q
 
 def search(request):
    try:
-    searchRequest = request.POST.get('search') # get the search term from the search bar where the name of the input field is search.
+    # get the search term from the search bar where the name of the input field is search.
+    searchRequest = request.POST.get('search') 
     
     #search in the job title and the job description fields of the table if they contain the search term. 
     searchable = Q(jobTitle__icontains= searchRequest) | Q(jobDescription__icontains= searchRequest)  
-
-    result = JobDetails.objects.filter(searchable) # get all the results containting the search term.
-    
-     return render(request, 'app/search.html', {"result":result}) # send the result to a template called search.html to process the result their. 
    
-  except:
-       return redirect("index") # get back to the homepage if an error occurs. 
+    # get all the results containting the search term.
+    result = JobDetails.objects.filter(searchable)
+          
+    # send the result to a template called search.html to process the result their. 
+    return render(request, 'app/search.html', {"result":result}) 
+      # send the result to a template called search.html to process the result their. 
+   
+   except:
+    return redirect("index") # get back to the homepage if an error occurs. 
